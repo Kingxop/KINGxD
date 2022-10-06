f4a89578967db5b3a0c6ae2bce3795fa94f82b63
Kingxop <95347735+Kingxop@users.noreply.github.com>
2022-10-06 20:31:28 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -9,89 +9,108 @@ from pyrogram import idle
 from . import (RiZoeL, RiZoeL2, RiZoeL3, RiZoeL4, RiZoeL5, 
                 RiZoeL6, RiZoeL7, RiZoeL8, RiZoeL9, RiZoeL10, 
                 RiZoeL11, RiZoeL12, RiZoeL13, RiZoeL14, RiZoeL15, 
-                RiZoeL16, RiZoeL17, RiZoeL18, RiZoeL19, RiZoeL20, hl)
+                RiZoeL16, RiZoeL17, RiZoeL18, RiZoeL19, RiZoeL20, RiZoeL21, RiZoeL22, RiZoeL23, RiZoeL24, RiZoeL25, hl)
 
 if RiZoeL:
     RiZoeL.start()
     RiZoeL.join_chat("FUNZONE_xD")
-    RiZoeL.join_chat("FUNZONE_xD")
+    RiZoeL.join_chat("ABOUT_KING_X")
 if RiZoeL2:
     RiZoeL2.start()
     RiZoeL2.join_chat("FUNZONE_xD")
-    RiZoeL2.join_chat("FUNZONE_xD")
+    RiZoeL2.join_chat("ABOUT_KING_X")
 if RiZoeL3:
     RiZoeL3.start()
     RiZoeL3.join_chat("FUNZONE_xD")
-    RiZoeL3.join_chat("FUNZONE_xD")
+    RiZoeL3.join_chat("ABOUT_KING_X")
 if RiZoeL4:
     RiZoeL4.start()
     RiZoeL4.join_chat("FUNZONE_xD")
-    RiZoeL4.join_chat("FUNZONE_xD")
+    RiZoeL4.join_chat("ABOUT_KING_X")
 if RiZoeL5:
     RiZoeL5.start()
     RiZoeL5.join_chat("FUNZONE_xD")
-    RiZoeL5.join_chat("FUNZONE_xD")
+    RiZoeL5.join_chat("ABOUT_KING_X")
 if RiZoeL6:
     RiZoeL6.start()
     RiZoeL6.join_chat("FUNZONE_xD")
-    RiZoeL6.join_chat("FUNZONE_xD")
+    RiZoeL6.join_chat("ABOUT_KING_X")
 if RiZoeL7:
     RiZoeL7.start()
     RiZoeL7.join_chat("FUNZONE_xD")
-    RiZoeL7.join_chat("FUNZONE_xD")
+    RiZoeL7.join_chat("ABOUT_KING_X")
 if RiZoeL8:
     RiZoeL8.start()
     RiZoeL8.join_chat("FUNZONE_xD")
-    RiZoeL8.join_chat("FUNZONE_xD")
+    RiZoeL8.join_chat("ABOUT_KING_X")
 if RiZoeL9:
     RiZoeL9.start()
     RiZoeL9.join_chat("FUNZONE_xD")
-    RiZoeL9.join_chat("FUNZONE_xD")
+    RiZoeL9.join_chat("ABOUT_KING_X")
 if RiZoeL10:
     RiZoeL10.start()
     RiZoeL10.join_chat("FUNZONE_xD")
-    RiZoeL10.join_chat("FUNZONE_xD")
+    RiZoeL10.join_chat("ABOUT_KING_X")
 if RiZoeL11:
     RiZoeL11.start()
     RiZoeL11.join_chat("FUNZONE_xD")
-    RiZoeL11.join_chat("FUNZONE_xD")
+    RiZoeL11.join_chat("ABOUT_KING_X")
 if RiZoeL12:
     RiZoeL12.start()
     RiZoeL12.join_chat("FUNZONE_xD")
-    RiZoeL12.join_chat("FUNZONE_xD")
+    RiZoeL12.join_chat("ABOUT_KING_X")
 if RiZoeL13:
     RiZoeL13.start()
     RiZoeL13.join_chat("FUNZONE_xD")
-    RiZoeL13.join_chat("FUNZONE_xD")
+    RiZoeL13.join_chat("ABOUT_KING_X")
 if RiZoeL14:
     RiZoeL14.start()
     RiZoeL14.join_chat("FUNZONE_xD")
-    RiZoeL14.join_chat("FUNZONE_xD")
+    RiZoeL14.join_chat("ABOUT_KING_X")
 if RiZoeL15:
     RiZoeL15.start()
     RiZoeL15.join_chat("FUNZONE_xD")
-    RiZoeL15.join_chat("FUNZONE_xD")
+    RiZoeL15.join_chat("ABOUT_KING_X")
 if RiZoeL16:
     RiZoeL16.start()
     RiZoeL16.join_chat("FUNZONE_xD")
-    RiZoeL16.join_chat("FUNZONE_xD")
+    RiZoeL16.join_chat("ABOUT_KING_X")
 if RiZoeL17:
     RiZoeL17.start()
     RiZoeL17.join_chat("FUNZONE_xD")
-    RiZoeL17.join_chat("FUNZONE_xD")
+    RiZoeL17.join_chat("ABOUT_KING_X")
 if RiZoeL18:
     RiZoeL18.start()
     RiZoeL18.join_chat("FUNZONE_xD")
-    RiZoeL18.join_chat("FUNZONE_xD")
+    RiZoeL18.join_chat("ABOUT_KING_X")
 if RiZoeL19:
     RiZoeL19.start()
     RiZoeL19.join_chat("FUNZONE_xD")
-    RiZoeL19.join_chat("FUNZONE_xD")
+    RiZoeL19.join_chat("ABOUT_KING_X")
 if RiZoeL20:
     RiZoeL20.start()
     RiZoeL20.join_chat("FUNZONE_xD")
-    RiZoeL20.join_chat("FUNZONE_xD")
-
+    RiZoeL20.join_chat("ABOUT_KING_X")
+if RiZoeL21:
+    RiZoeL21.start()
+    RiZoeL21.join_chat("FUNZONE_xD")
+    RiZoeL21.join_chat("ABOUT_KING_X")
+if RiZoeL22:
+    RiZoeL22.start()
+    RiZoeL22.join_chat("FUNZONE_xD")
+    RiZoeL22.join_chat("ABOUT_KING_X")
+if RiZoeL21:
+    RiZoeL23.start()
+    RiZoeL23.join_chat("FUNZONE_xD")
+    RiZoeL23.join_chat("ABOUT_KING_X")
+if RiZoeL24:
+    RiZoeL24.start()
+    RiZoeL24.join_chat("FUNZONE_xD")
+    RiZoeL24.join_chat("ABOUT_KING_X")
+if RiZoeL25:
+    RiZoeL25.start()
+    RiZoeL25.join_chat("FUNZONE_xD")
+    RiZoeL25.join_chat("ABOUT_KING_X")
 print("Your Spam Bots Successfully Deployed ✅")
 print("Visit -! @FUNZONE_xD")
 
